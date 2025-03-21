5bb623aaddb1be4ff3e864d6a1ed7f6cc42c45cd
GihanJY <123gihanjaya123@gmail.com>
2025-03-21 09:37:34 +0530

    Before login screen update
    - Improved the screen structure according to the Figma design.
    - Added a navigation button to access the teacher login screen.
    - SpaceEvenly ensure the screen is responsive.

---
+++ lib/Screens/before_login_screen.dart
@@ -0,0 +1,129 @@
+import 'package:edu_mate/Screens/LoginScreen.dart';
+import 'package:flutter/material.dart';
+
+class Beforeloginscreen extends StatefulWidget {
+  const Beforeloginscreen({super.key});
+
+  @override
+  State<Beforeloginscreen> createState() => _BeforeloginscreenState();
+}
+
+class _BeforeloginscreenState extends State<Beforeloginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF13134C),
+              Color(0xFF13134C),
+              Color(0xFF2D2DB2),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Login As",
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFF0000),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 55, vertical: 10),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Loginscreen(role: "admin"),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Admin",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF5342FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Loginscreen(role: "student"),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Student",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFD6741D),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 48, vertical: 10),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Loginscreen(role: "teacher"),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Teacher",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Image.asset(
+                  "assets/images/AppLogo.png",
+                  height: 100,
+                  width: 100,
+                ),
+                Text(
+                  "EduMate",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
